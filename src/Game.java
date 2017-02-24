389055521fcdf47a7ae24a6ca94ffaa0f9db92d3
Marcel <Marcel@DESKTOP-TG879BB>
2017-02-24 17:11:20 +0100

    Basic class with Builder for a game

---
+++ src/Game.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+/**
+ * Class to save all significant data for one game
+ * 
+ * @author Marcel
+ *
+ */
+public class Game {
+	
+	public Game()
+	{
+		try
+		{
+			//one board has a static size of 8x8
+			this.board = new int[8][8];
+			this.moves = new ArrayList<Move>();
+		}
+		catch (Error r)
+		{
+			System.out.println(r.getMessage());
+		}
+	}
+
+	/**
+	 * the current position on the board
+	 */
+	int[][] board;
+	
+	/**
+	 * a List with all moves that the players have made
+	 */
+	ArrayList<Move> moves;
+	
+	/**
+	 * Method to generate a Notation String
+	 * 
+	 * @return the Notation
+	 */
+	public String getNotation ()
+	{
+		//code to be added
+		//the code should take all moves of the game and then generate a String that represents the Notation
+		return null;
+	}
+}
