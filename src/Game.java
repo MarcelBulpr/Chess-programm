8b3f5f50269b5c646296e91d724b9fad74be260d
Marcel Baumann <littellittel@gmx.de>
2017-03-31 16:17:34 +0200

    added methods to convert coordinates

--- src/Game.java
+++ src/Game.java
@@ -3,12 +3,12 @@ import java.awt.Point;
 
 /**
  * Class to save all significant data for one game
- * 
+ *
  * @author Marcel
  *
  */
 public class Game {
-	
+
 	public Game()
 	{
 		try
@@ -27,15 +27,15 @@ public class Game {
 	 * the current position
 	 */
 	Position position;
-	
+
 	/**
 	 * a List with all moves that the players have made
 	 */
 	ArrayList<Move> moves;
-	
+
 	/**
 	 * Method to generate a Notation String
-	 * 
+	 *
 	 * @return the Notation
 	 */
 	public String getNotation ()
@@ -44,10 +44,10 @@ public class Game {
 		//the code should take all moves of the game and then generate a String that represents the Notation
 		return null;
 	}
-	
+
 	/**
 	 * Copy the game class to another ram address
-	 * 
+	 *
 	 * @param game the game in another ram address
 	 */
 	public void copy(Game game)
@@ -56,7 +56,7 @@ public class Game {
 		{
 			//copy the position
 			this.position.copy(game.position);
-			
+
 			//add each move to the moves Array
 			for (int i = 0; i < game.moves.size(); i++)
 				this.moves.add(game.moves.get(i));
@@ -78,7 +78,7 @@ public class Game {
 				this.moves.add(move);
 				return true;
 			}
-			
+
 			return false;
 		}
 		catch(Error r)
@@ -86,5 +86,69 @@ public class Game {
 			System.out.print(r.getMessage());
 			return false;
 		}
+	}
+
+	/**
+	 * methodeto get the game coordinates (like d4) from array coordinates
+	 *
+	 * @param coordinates the coordinates from the array
+	 * @return String containing a square name (like d4)
+	 */
+	public String getGameCoordinates(Point coordinates)
+	{
+		String gameCoordinates = "";
+		try
+		{
+		//check the x row
+		switch(coordinates.x)
+		{
+			case 0: gameCoordinates += "a"; break;
+			case 1: gameCoordinates += "b"; break;
+			case 2: gameCoordinates += "c"; break;
+			case 3: gameCoordinates += "d"; break;
+			case 4: gameCoordinates += "e"; break;
+			case 5: gameCoordinates += "f"; break;
+			case 6: gameCoordinates += "g"; break;
+			case 7: gameCoordinates += "h"; break;
+		}
+		//calculate the column
+		gameCoordinates += (8 - coordinates.y);
+		return gameCoordinates;
+		}
+		catch(Exception r)
+		{
+			System.out.print(r.getMessage());
+			return null;
+		}
+	}
+
+	/**
+	 * method to get the Array coordinates from a square name (like d4)
+	 *
+	 * @param coordinates the square name
+	 * @return Point with the Array coordinates
+	 */
+	public Point getArrayCoordinates(String coordinates)
+	{
+		//check if the coordinates string really consists of 2 chars
+		assert coordinates.length() == 2;
+
+		Point arrayCoordinates = new Point();
+
+		//check the letter (a-h)
+		switch (coordinates.charAt(0)) {
+			case 'a': arrayCoordinates.x = 0; break;
+			case 'b': arrayCoordinates.x = 1; break;
+			case 'c': arrayCoordinates.x = 2; break;
+			case 'd': arrayCoordinates.x = 3; break;
+			case 'e': arrayCoordinates.x = 4; break;
+			case 'f': arrayCoordinates.x = 5; break;
+			case 'g': arrayCoordinates.x = 6; break;
+			case 'h': arrayCoordinates.x = 7; break;
+		}
+		//calculate the column
+		arrayCoordinates.y = 7 - (Character.getNumericValue(coordinates.charAt(1))-1);
+
+		return arrayCoordinates;
 	}
 }
