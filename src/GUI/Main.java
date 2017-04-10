af42c652df447bd5fd114fd9b502578af1350809
Nils Kruse <nikru979@gmail.com>
2017-04-10 20:39:06 +0200

    The Main-class for the GUI

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,16 +1,17 @@
+//@author Nils Kruse 
 package GUI;
 
 import java.io.IOException;
 
-import javafx.application.Application;
+import javafx.application.Application; //Importing the fxml libraries
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	private Stage primaryStage;
-	private Pane mainLayout;
+	private Stage primaryStage; // Defining the stage 
+	private Pane mainLayout;    // and the main Layout
 	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
@@ -20,11 +21,11 @@ public class Main extends Application {
 		
 	}
 
-	private void showMainView() throws IOException {
-	FXMLLoader loader = new FXMLLoader();
+	private void showMainView() throws IOException { //Function to make the fxml code able to load
+	FXMLLoader loader = new FXMLLoader(); 
 	loader.setLocation(Main.class.getResource("view/MainView.fxml"));
 	mainLayout = loader.load();		
-	Scene scene = new Scene(mainLayout); 
+	Scene scene = new Scene(mainLayout); //definition of what will be loaded
 	primaryStage.setScene(scene);
 	primaryStage.show();
 	}
