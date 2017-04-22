b9852743fa297d44d1fb508c2969a1289f19ed4b
MrPolemonium <nikru979@gmail.com>
2017-04-22 23:20:05 +0200

    Created a Contoller-Class for the GUI

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -2,11 +2,15 @@
 
 package GUI;
 
+//import java.io.FileInputStream;
 import java.io.IOException;
 
 import javafx.application.Application; //Importing the fxml libraries
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -20,16 +24,33 @@ public class Main extends Application {
 		this.primaryStage.setTitle("Chess");
 		showMainView();
 		
+//		FileInputStream input = new FileInputStream("@../../rook-black.png");
+//		Image image = new Image(input);
+//		ImageView imageView = new ImageView(image);
+		 GridPane gridPane = new GridPane();
+		 gridPane.setPadding(new Insets (0,0,0,0));
+		 gridPane.setPrefSize(600, 600);
+
+
 	}
 
 	private void showMainView() throws IOException { //Function to make the fxml code able to load
 	FXMLLoader loader = new FXMLLoader(); 
 	loader.setLocation(Main.class.getResource("view/MainView.fxml"));
-	mainLayout = loader.load();		
+	
+	mainLayout = loader.load();	
+	
+	MainWindowController mainWindowController = loader.getController();
+	mainWindowController.setMain(this);
+	
 	Scene scene = new Scene(mainLayout); //definition of what will be loaded
+	
 	primaryStage.setScene(scene);
 	primaryStage.show();
+
 	}
+	
+	
 
 	public static void main(String[] args) {
 		launch(args);
---
+++ src/GUI/MainWindowController.java
@@ -0,0 +1,13 @@
+package GUI;
+
+public class MainWindowController {
+
+
+
+	private Main main;
+	
+	public void setMain(Main main){
+		this.main = main;
+	}
+
+}
